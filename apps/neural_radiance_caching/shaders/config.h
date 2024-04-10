06b94f22f395fafc6834e800c417856eb76aba92
Jamie Chen <jamiechenang@gmail.com>
2024-04-09 21:18:34 -0400

    add preproc directives to make intellisense happy

--- apps/neural_radiance_caching/shaders/config.h
+++ apps/neural_radiance_caching/shaders/config.h
@@ -35,6 +35,15 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
+#if defined __INTELLISENSE__
+#define __CUDACC__
+#define __NVCC__
+#define __CUDA_ARCH__ 890
+#include <cuda_runtime.h>
+#include <optix_host.h>
+#include <optix_device.h>
+#endif
+
 // Used to shoot rays without distance limit.
 #define RT_DEFAULT_MAX 1.0e16f
 
